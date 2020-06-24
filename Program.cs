35d7918ebbb17ca4a71784be149e6a15a3a6fa9d
Shjani49 <shjani49@gmail.com>
2020-06-24 15:15:35 -0600

    Add an assignment; output concatenated hello string.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Shivani!");
+            // Two slashes mark the beginning of a single-line comment.
+            // We are assigning a string value to a variable.
+            string myName = "Shivani Jani";
+            /* We begin multi-line comments eith a forward-slash and asterisk...
+
+            Symbols between values or evaluations are considerd "operators."
+            Below we are using the addition symbol to concatenate two strings.
+            (Concatenatations means gluing two strings, or texts, together.)
+
+            ...and We end multi-line comments with an asterisk followed by a forward-slash */
+            Console.WriteLine("Hello, " + myName);
         }
     }
 }
