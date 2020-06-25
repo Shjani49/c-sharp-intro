1a1e5f8bacfc2e47098c70614beecb8081ee646f
Shjani49 <shjani49@gmail.com>
2020-06-25 11:47:56 -0600

    Add Logiacl operators (||,&&); Add Comparative operators (==,!=,<,>,<=,>=).

--- Program.cs
+++ Program.cs
@@ -7,64 +7,53 @@ namespace c_sharp_intro
     {
         static void Main(string [] args)
         {
-            // "=" is the assignment operator!
-            // String datatype is used for text.
-            string mystring = "This is a string";
-            Console.WriteLine("Your Message is:"+ mystring );
-            //Integer datatype is used for whole numbers(no decimals.)
-            int myInt = 42;
-            Console.WriteLine("Your Number is:"+ myInt );
-            // Double datatype is used for decimal point numbers.
-            double myDouble = 33.666;
-            Console.WriteLine("Your Number is:"+ myDouble );
-            //Boolean datatype is used for TRUE/FALSE values - think binary!
-            bool myBool = true;
+            //Logical Operators
+            // OR will evaluate to TRUE if EITHER side id TRUE.
+            Console.WriteLine( true || true ); // || is OR // True
+            Console.WriteLine( false || true) ; // || is OR // True
+            Console.WriteLine( false || false ); // False
 
-            // Subtraction operator is "-" (minus)
-            Console.WriteLine(myInt - 10); //32
-
-            //Mutiplication operator is "*" (multiply)
-            Console.WriteLine(myInt*2); //84
-
-            //Division operator is "/"(division")
-            Console.WriteLine(myInt/2); //21
-
-            // Modulus operator is "%"n(percent sign)
-            Console.WriteLine(myInt % 13); //3
-
-            // Addition operator is "+" (plus)
-            // ***WAIT ! this is ALSO the concatenation opertor.
-            Console.WriteLine(myInt + 8); //50
-
-            Console.WriteLine("3" + "3"); //33
-
-            Console.WriteLine("3" + 3); //??? 33
-            Console.WriteLine(3 + "3"); ///??? 33
-            Console.WriteLine(6 + 3 + "3"); ///??? 93
-
-            //Output our variable.
-            Console.WriteLine(myInt); // 42
-
-            // A new assignment on existng variable.
-            myInt = myInt +2 ;// 44
-            Console.WriteLine(myInt); // 44
-
-            //myInt = myInt + "2"; //We can't turn myInt into a string
-            // We told C# that myInt would always be an int!
-
-            myInt++; //The same as typing: myInt = myInt +1 ;
-            Console.WriteLine(myInt); // 45
-            myInt--; //The same as typing: myInt = myInt - 1 ;
-            Console.WriteLine(myInt); // 44
-
-            myInt += 3; // The same as typing: myInt = myInt +3 ;
-            Console.WriteLine(myInt); // 47
-
-            myInt -= 5; // The same as typing: myInt = myInt -5 ;
-            Console.WriteLine(myInt); // 42
+            // AND will evaluate to TRUE only if BOTH side id TRUE.
+            Console.WriteLine( true && true ); // && is AND // True
+            Console.WriteLine( false && true ); // && is AND // False
+            Console.WriteLine( false && false ); // False
 
 
+            // How does this behave with different data-types?
+            //Console.WriteLine(true && 1); // IT DOESN'T !
 
+            //  Comparative operators...
+            // "Equal to"check.
+            Console.WriteLine( false == false ); // True
+            Console.WriteLine( true == false ); // False
+            Console.WriteLine( 2.25 == 2.25 ); // True
+            Console.WriteLine( 2.25 == 3.14 ); // False
+            //Console.WriteLine( 3.14 == "3.14" ); // ??? can't be apllied because two different data types !
+
+
+            // "Greater than" check
+                Console.WriteLine( 6 > 7 ); // False
+                Console.WriteLine( 10 > 5 ); // True          
+
+            // "Less than" check
+                Console.WriteLine( 6 < 7 ); // True
+                Console.WriteLine( 10 < 5 ); // False         
+            
+            // "Greater-than-or-equal-to" check
+                Console.WriteLine( 5 >= 5 ); // True
+                Console.WriteLine( 10 >= 5 ); // True          
+                Console.WriteLine( 4 >= 5 ); // False
+
+            // "Less-than-or-equal-to" check
+                Console.WriteLine( 5 <= 5 ); // True
+                Console.WriteLine( 10 <= 5 ); // False
+                Console.WriteLine( 4 <= 5 ); // True
+
+            // "NOt-Equal-to" check
+               Console.WriteLine( false != false ); // False
+               Console.WriteLine( true != false ); // True 
+               Console.WriteLine( 5 != 5 ); // False
+               Console.WriteLine( 10 != 5 ); // True
 
         }
     }
