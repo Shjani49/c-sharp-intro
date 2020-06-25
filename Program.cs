e0920e8410cc624f4fd4807cc0df58624af21339
Shjani49 <shjani49@gmail.com>
2020-06-25 14:05:35 -0600

    Add Multiplication, Subtraction, and Addition using If, Else..If, and Else

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace c_sharp_intro
     {
         static void Main(string [] args)
         {
-            Console.WriteLine("Please tell us if you want to add or subtract: (add/ subtract)");
+            Console.WriteLine("Please tell us if you want to add,subtract, or multiply: (Add/ Subtract/ multiply)");
             string userOperator = Console.ReadLine();
             // Check if the use typed: add
-            if( userOperator == "add")
+            if( userOperator == "Add")
             {
                 Console.WriteLine("Please enter the first number: ");
                 // @Link : https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
@@ -23,7 +23,7 @@ namespace c_sharp_intro
                 Console.WriteLine("{0} + {1} = {2}",firstNum, secondNum, result);
             }
             // Check "else" if user did not typed 
-            else if( userOperator == "subtract")
+            else if( userOperator == "Subtract")
             {
                 Console.WriteLine("Please enter the first number: ");
                 // @Link : https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
@@ -35,6 +35,23 @@ namespace c_sharp_intro
                 int result = firstNum - secondNum ;
                 Console.WriteLine("{0} - {1} = {2}",firstNum, secondNum, result);
                 
+            }
+            else if( userOperator == "Multiply")
+            {
+                Console.WriteLine("Please enter the first number: ");
+                // @Link : https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+                int firstNum = Int32.Parse(Console.ReadLine()); // Convert string to integer.
+
+                Console.WriteLine("Please enter the second number: ");
+                int secondNum = Int32.Parse(Console.ReadLine()); // Convert string to integer.
+                // Calculate the result.
+                int result = firstNum * secondNum ;
+                Console.WriteLine("{0} * {1} = {2}",firstNum, secondNum, result);
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator entered.");
             }
         }
     }
